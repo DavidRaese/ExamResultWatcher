80a602b39491b46c40998ad749a2ccf88ab00a26
David Raese <david.raese@edu.fh-joanneum.at>
2021-09-10 17:32:20 +0200

    added login feature

--- src/pageScraper.js
+++ src/pageScraper.js
@@ -1,9 +1,15 @@
+const secrets = require("./secrets");
+
 const scraperObject = {
   url: "https://online.fh-joanneum.at/",
   async scraper(browser) {
     let page = await browser.newPage();
     console.log(`Navigating to ${this.url}...`);
     await page.goto(this.url);
+    await page.waitForSelector("input[name='username']", { visible: true });
+    await page.type("input[name='username']", secrets.username);
+    await page.type("input[name='password']", secrets.password);
+    await page.click("#id_brm-pm-dtop_login_submitbutton");
   },
 };
 
