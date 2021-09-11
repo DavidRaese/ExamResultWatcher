887eec67a2bd36a1467a8c53e5e0bec5d5a48c03
David Raese <david.raese@edu.fh-joanneum.at>
2021-09-11 06:41:24 +0200

    first working bot

--- src/pageScraper.js
+++ src/pageScraper.js
@@ -1,4 +1,6 @@
 const secrets = require("./secrets");
+const fs = require("fs");
+const dayjs = require("dayjs");
 
 const scraperObject = {
   url: "https://online.fh-joanneum.at/",
@@ -26,10 +28,51 @@ const scraperObject = {
       return document.querySelectorAll("ca-list-entry").length;
     });
 
-    if (secrets.numberOfScores === currentNumberOfScores) {
-      console.log("No new scores");
+    if (secrets.numberOfScores == -1) {
+      updateNumberOfScores(currentNumberOfScores);
+      appendToLogFile(
+        `Initial number of scores where set to ${currentNumberOfScores}`
+      );
+    } else if (secrets.numberOfScores < currentNumberOfScores) {
+      updateNumberOfScores(currentNumberOfScores);
+      appendToLogFile(
+        `FOUND NEW RESULT!!!. Number of results were set to ${currentNumberOfScores} in secrets.js`
+      );
+    } else {
+      appendToLogFile(`no new results`);
     }
   },
+};
+
+const updateNumberOfScores = (newNumberOfScores) => {
+  try {
+    fs.readFile("./src/secrets.js", "utf8", (err, data) => {
+      const updatedSecretsObject = data.replace(
+        new RegExp("numberOfScores: -?[0-9]+"),
+        `numberOfScores: ${newNumberOfScores}`
+      );
+
+      fs.writeFile(
+        "./src/secrets.js",
+        updatedSecretsObject,
+        "utf8",
+        (err) => {}
+      );
+    });
+  } catch (err) {
+    throw new Error(
+      "Couldn't find secrets.js file, please provide on and enter correct data"
+    );
+  }
+};
+
+const appendToLogFile = (string) => {
+  const dateString = dayjs().format("D.M.YYYY H:m");
+  const completeString = `${dateString}: ${string}\n`;
+
+  fs.appendFile("./log.txt", completeString, (err) => {
+    if (err) throw err;
+  });
 };
 
 module.exports = scraperObject;
